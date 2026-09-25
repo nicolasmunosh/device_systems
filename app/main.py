a0ed37750130150045d378323968eed0d221c8b6
nicolasnunosh <nicomunosh@gmail.com>
2026-09-24 21:30:56 -0500

    feature: Alembic configurado migracion devices loans

--- app/main.py
+++ app/main.py
@@ -1,34 +1,38 @@
 # =============================================
-# main.py - device_systems v3.0.0
+# main.py - device_systems v4.0.0
 # =============================================
 
 from fastapi import FastAPI
 from app.database.connection import engine, Base
-from app.routes.user_routes import router
-import app.models.user_model  # noqa: F401 — registra el modelo
+from app.routes.user_routes   import router as user_router
+from app.routes.device_routes import router as device_router
+from app.routes.loan_routes   import router as loan_router
 
-# Crear las tablas en la base de datos al iniciar
+import app.models.user_model    # noqa
+import app.models.device_model  # noqa
+import app.models.loan_model    # noqa
+
+# Crear tablas al iniciar
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="device_systems API",
-    description="API REST para gestión de usuarios con persistencia SQLAlchemy.",
-    version="3.0.0",
-    contact={
-        "name":  "nicolasmunosh",
-        "url":   "https://github.com/nicolasmunosh",
-    },
+    description="API REST para gestión de usuarios, dispositivos y préstamos con SQLAlchemy y Alembic.",
+    version="4.0.0",
+    contact={"name": "nicolasmunosh", "url": "https://github.com/nicolasmunosh"},
 )
 
-app.include_router(router)
+app.include_router(user_router)
+app.include_router(device_router)
+app.include_router(loan_router)
 
 
 @app.get("/", tags=["Root"], summary="Inicio")
 def inicio():
     return {
         "app":      "device_systems",
-        "version":  "3.0.0",
-        "database": "SQLite - device_systems.db",
+        "version":  "4.0.0",
+        "recursos": ["/users", "/devices", "/loans"],
         "docs":     "/docs",
         "redoc":    "/redoc"
     }
